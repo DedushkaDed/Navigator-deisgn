12ffadfca3c34e5aa6b80ff05ac8b7a66bdeacc0
Gleb <glebstuck@yandex.ru>
2021-03-26 15:06:48 +0500

    Готовый слайдер 3.0

--- js/swiper.js
+++ js/swiper.js
@@ -2,8 +2,25 @@ var swiper = new Swiper('.swiper-container', {
   slidesPerView: 3,
   spaceBetween: 30,
   freeMode: true,
+
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
+  // Бесконечный слайдер
+	loop: true,
+  
+  // Брейк поинты (адаптив)
+	// Ширина экрана
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		480: {
+			slidesPerView: 2,
+		},
+		992: {
+			slidesPerView: 3,
+		}
+	},
 });
