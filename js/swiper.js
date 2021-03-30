5335448a031fcb6e736374951392aa95a209719f
Gleb <glebstuck@yandex.ru>
2021-03-30 16:56:23 +0500

    А что у нас нового - готовый 1 блок

--- js/swiper.js
+++ js/swiper.js
@@ -1,64 +1,92 @@
-var swiper = new Swiper('.our-offers-slider', {
+var swiper = new Swiper(".our-offers-slider", {
   slidesPerView: 3,
   spaceBetween: 30,
   freeMode: true,
 
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
   // Бесконечный слайдер
-	loop: true,
-  
+  loop: true,
+
   // Брейк поинты (адаптив)
-	// Ширина экрана
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-		},
-		480: {
-			slidesPerView: 2,
-		},
-		992: {
-			slidesPerView: 3,
-		}
-	},
+  // Ширина экрана
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
 });
 
-
 // О компании - слайдер.
-var swiper2 = new Swiper('.image-slider', {
-	slidesPerView: 1,
-	spaceBetween: 0,
-	freeMode: true,
-  
-	pagination: {
-	  el: '.swiper-pagination',
-	  clickable: true,
-	},
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev'
-	},
-	// Бесконечный слайдер
-	  loop: true,
-  });
+var swiper2 = new Swiper(".image-slider", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  freeMode: true,
 
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // Бесконечный слайдер
+  loop: true,
+});
 
 //   Почему выбирают именно нас?
-var swiper3 = new Swiper('.main__zemlya-u-nas__wrapper__items-2__main-item__slider', {
-	slidesPerView: 1,
-	spaceBetween: 0,
-	freeMode: true,
-  
-	pagination: {
-	  el: '.swiper-pagination',
-	  clickable: true,
-	},
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev'
-	},
-	// Бесконечный слайдер
-	  loop: true,
-  });
+var swiper3 = new Swiper(
+  ".main__zemlya-u-nas__wrapper__items-2__main-item__slider",
+  {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    freeMode: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    // Бесконечный слайдер
+    loop: true,
+  }
+);
+
+
+var swiper3 = new Swiper(".main__what-new__slider-container__swiper-container", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  // Бесконечный слайдер
+  loop: true,
+  // Брейк поинты (адаптив)
+  // Ширина экрана
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    480: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView: 3,
+    },
+  },
+});
