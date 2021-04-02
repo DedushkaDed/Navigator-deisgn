43988f5fb11e642e55b67d01cba63ed5590bcfef
Gleb <glebstuck@yandex.ru>
2021-04-02 10:56:37 +0500

    Карта готова

--- js/main.js
+++ js/main.js
@@ -1,54 +1,59 @@
-
 // Модальные окна
-var $formPopup = document.getElementById('popup');
-var $formPopupSucces = document.getElementById('popup-2');
+var $formPopup = document.getElementById("popup");
+var $formPopupSucces = document.getElementById("popup-2");
 // Слайдер с предложениями
-var $ourOffersSlider = document.querySelector('.our-offers-slider');
-var $yMapOurOffers = document.querySelector('.offers-slider-container__yMap');
-var $ourOffersRightItem = document.querySelector('.main__our-offers__items__item-right');
-var $yMapRight = document.querySelector('.yMap-right');
-
+var $ourOffersSlider = document.querySelector(".our-offers-slider");
+var $yMapOurOffers = document.querySelector(".offers-slider-container__yMap");
+var $ourOffersRightItem = document.querySelector(
+  ".main__our-offers__items__item-right"
+);
+var $yMapRight = document.querySelector(".yMap-right");
 
 function showContactCall() {
-    $formPopup.style.display = 'block';
-    $formPopup.style.opacity = 1;
+  $formPopup.style.display = "block";
+  $formPopup.style.opacity = 1;
 }
 function closeContactForm() {
-    // 1
-    $formPopup.style.display = 'none';
-    $formPopup.style.opacity = 0;
-    // 2
-    $formPopupSucces.style.display = 'none';
-    $formPopupSucces.style.opacity = 0;
-
+  // 1
+  $formPopup.style.display = "none";
+  $formPopup.style.opacity = 0;
+  // 2
+  $formPopupSucces.style.display = "none";
+  $formPopupSucces.style.opacity = 0;
 }
 function showContactCallSucess() {
-    // 1
-    $formPopup.style.display = 'none';
-    $formPopup.style.opacity = 0;
-    // 2
-    $formPopupSucces.style.display = 'block';
-    $formPopupSucces.style.opacity = 1;
+  // 1
+  $formPopup.style.display = "none";
+  $formPopup.style.opacity = 0;
+  // 2
+  $formPopupSucces.style.display = "block";
+  $formPopupSucces.style.opacity = 1;
 }
 function showMap() {
-    // Скрываем слайдер с предложениями
-    $ourOffersSlider.style.display = 'none';
-    // Скрываем переключение слайдов с предложениями
-    $ourOffersRightItem.style.display = 'none';
-    // Отображаем блок yandexMap
-    $yMapOurOffers.style.display = 'block';
+  if (window.matchMedia("(max-width: 320px)").matches) {
+    $yMapRight.style.display = "none";
+  } else {
     // Отображаем 'Подобрать участок'
-    $yMapRight.style.display = 'block';
-
+    $yMapRight.style.display = "block";
+  }
+  // Скрываем слайдер с предложениями
+  $ourOffersSlider.style.display = "none";
+  // Скрываем переключение слайдов с предложениями
+  $ourOffersRightItem.style.display = "none";
+  // Отображаем блок yandexMap
+  $yMapOurOffers.style.display = "block";
 }
 function showOffers() {
-    // Скрываем блок yandexMap
-    $yMapOurOffers.style.display = 'none';
-    // Скрываем 'Подобрать участок'
-    $yMapRight.style.display = 'none';
-    // Показываем слайдер с предложениями
-    $ourOffersSlider.style.display = 'block';
+  if (window.matchMedia("(max-width: 320px)").matches) {
+    $ourOffersRightItem.style.display = "none";
+  } else {
     // Показываем переключение слайдов с предложениями
-    $ourOffersRightItem.style.display = 'flex';
-    
+    $ourOffersRightItem.style.display = "block";
+  }
+  // Скрываем блок yandexMap
+  $yMapOurOffers.style.display = "none";
+  // Скрываем 'Подобрать участок'
+  $yMapRight.style.display = "none";
+  // Показываем слайдер с предложениями
+  $ourOffersSlider.style.display = "block";
 }
